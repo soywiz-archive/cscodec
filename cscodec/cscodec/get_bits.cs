ba06cab95115476e0d7df99d5b49bba469d6d7b8
soywiz <soywiz@gmail.com>
2012-02-14 05:01:17 +0100

    - First commit

---
+++ cscodec/cscodec/get_bits.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cscodec
+{
+	/// <summary>
+	/// get_bits.h
+	/// </summary>
+	public partial class VLC
+	{
+		public Object table;
+
+		int bits;
+		//VLC_TYPE (*table)[2]; ///< code, bits
+		int table_size, table_allocated;
+
+		internal void init_vlc(int HGAINVLCBITS, int p, byte[] ff_wma_hgain_huffbits, int p_2, int p_3, ushort[] ff_wma_hgain_huffcodes, int p_4, int p_5, int p_6)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void free_vlc()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal unsafe void init_vlc(int VLCBITS, int n, byte* table_bits, int p, int p_2, uint* table_codes, int p_3, int p_4, int p_5)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal unsafe void init_coef_vlc(ushort** p, float** p_2, ushort** p_3, CoefVLCTable coefVLCTable)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void init_vlc(int VLCBITS, int n, byte[] table_bits, int p, int p_2, uint[] table_codes, int p_3, int p_4, int p_5)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void init_coef_vlc(ushort p, float p_2, ushort p_3, global::cscodec.wma.wma.CoefVLCTable globalcscodecwmawmaCoefVLCTable)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ cscodec/cscodec/utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cscodec
+{
+	public struct IntFloat
+	{
+		private byte[] Value;
+
+		public float f
+		{
+			get
+			{
+				return BitConverter.ToSingle(Value, 0);
+			}
+			set
+			{
+				Value = BitConverter.GetBytes(value);
+			}
+		}
+
+		public uint v
+		{
+			get
+			{
+				return BitConverter.ToUInt32(Value, 0);
+			}
+			set
+			{
+				Value = BitConverter.GetBytes(value);
+			}
+		}
+	}
+}
